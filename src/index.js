8fd0499616dd6dd0b4d34fe214ef6c80b02c79c9
Jonathan <jonathanmoorevla@gmail.com>
2022-07-27 23:39:44 -0500

    Set up files, create basic layout and functions

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// index.js
+
+const searchForm = document.getElementById('artKeywordSearch')
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleSearchGet(e);
+    searchForm.reset()
+})
+function handleSearchGet(e) {
+    let searchInput = e.target.querySelector('#artSearch').value
+    
+    fetch(`https://api.artic.edu/api/v1/artworks/search?q=${searchInput}&query[term][is_public_domain]=true`)
+    .then(resp => resp.json())
+    .then(data => {
+        handleSearchCards(data.data)
+    })
+}
+function handleSearchCards(searchResults) {
+    let resultsUl = document.getElementById('results')
+    resultsUl.innerHTML = ''
+    let previewArr = []
+    searchResults.forEach(entry => {
+        //console.log(entry)
+        previewArr.push(entry.id)
+        let li = document.createElement('li')
+        li.id = entry.id
+        li.className = 'resultsElements'
+        li.innerHTML = `
+        <p class='titles'>${entry.title}</p>
+        `
+        li.addEventListener('mouseover', (e) => {
+            createPreview(e)
+        })
+        resultsUl.appendChild(li)
+    })
+    handlePreviewPrep(previewArr.join(','))
+}
+let previewObj = {}
+function handlePreviewPrep(input) {
+    fetch(`https://api.artic.edu/api/v1/artworks?ids=${input}&fields=id,title,thumbnail,place_of_origin,date_display,artist_title,medium_display`)
+    .then(resp => resp.json())
+    .then(data => {
+        previewObj = data
+    })   
+}
